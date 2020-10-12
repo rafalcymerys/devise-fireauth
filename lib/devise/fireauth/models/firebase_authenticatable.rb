d04b3878464aa5faaf073d66e3da32147abb1e1d
Rafal Cymerys <rafal@upsidelab.io>
2020-10-12 17:17:05 +0200

    Do not override session serialization methods

--- lib/devise/fireauth/models/firebase_authenticatable.rb
+++ lib/devise/fireauth/models/firebase_authenticatable.rb
@@ -11,35 +11,6 @@ module Devise
       end
 
       module ClassMethods
-        ####################################
-        # Overriden methods from Devise::Models::Authenticatable
-        ####################################
-
-        #
-        # This method is called from:
-        # Warden::SessionSerializer in devise
-        #
-        # It takes as many params as elements had the array
-        # returned in serialize_into_session
-        #
-        # Recreates a resource from session data
-        #
-        def serialize_from_session(id)
-          resource = self.new
-          resource.id = id
-          resource
-        end
-
-        #
-        # Here you have to return and array with the data of your resource
-        # that you want to serialize into the session
-        #
-        # You might want to include some authentication data
-        #
-        def serialize_into_session(record)
-          [record.id]
-        end
-
         def from_firebase(auth_hash)
           raise NotImplementedError,
             "#{self.name} model must implement class method `from_firebase'"
